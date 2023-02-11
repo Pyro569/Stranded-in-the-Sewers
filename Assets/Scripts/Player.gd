088628106a2717dc7ace1dad587b23c545171733
KobiSteve07 <109444953+KobiSteve07@users.noreply.github.com>
2023-02-10 20:26:31 -0500

    FRICTION!!!!!!!!!!!!!!!!!!!!!

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -3,13 +3,15 @@ signal hit
 
 export var speed = 400 #player speed pixels/sec EXPORT KEY ALLOWS TO SET VALUE IN INSPECTOR
 var screen_size
+var velocity = Vector2.ZERO #player movement vector
 
 func _ready(): #called when node enters scene tree
 	screen_size = get_viewport_rect().size
 
 
+
 func _process(delta): #function _process is called every frame
-	var velocity = Vector2.ZERO #player movement vector
+	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -21,6 +23,11 @@ func _process(delta): #function _process is called every frame
 		
 	if velocity.y > -9.8:
 		velocity.y += 0.1 #GRAVITY!!!!!!
+		
+	if velocity.x > 0:
+		velocity.x -= 0.5
+	elif velocity.x < 0:
+		velocity.x += 0.5 #FRICTION!!!!!
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
