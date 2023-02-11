fea19de66bd7408a651c7791ce6a87ce760794cb
Lost Fire Game Studios <79991107+LostFireStudios@users.noreply.github.com>
2023-02-10 22:48:45 -0500

    Added Things

---
+++ Assets/Scripts/Player2.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+const GRAVITY = 20
+const JUMPSPEED = 700
+const FLOOR = Vector2(0, -1)
+
+var velocity = Vector2.ZERO
+
+var maxSpeed = 300
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right2"):
+		velocity.x = +maxSpeed	
+	elif Input.is_action_pressed("move_left2"):
+		velocity.x = -maxSpeed
+	elif is_on_floor():
+		velocity.x = 0
+
+	
+	if Input.is_action_pressed("move_up2"):
+		if is_on_floor():
+			velocity.y = - JUMPSPEED	
+	
+	velocity.y += GRAVITY
+		
+	move_and_slide(velocity, FLOOR)
