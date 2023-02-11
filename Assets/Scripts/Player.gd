d2a6adf76eb05c04bfe59f204fbfb158493b9eb6
KobiSteve07 <109444953+KobiSteve07@users.noreply.github.com>
2023-02-10 21:00:18 -0500

    added framerate

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -10,7 +10,7 @@ func _ready(): #called when node enters scene tree
 
 
 
-func _process(delta): #function _process is called every frame
+func _process(frameLength): #function _process is called every frame, for frameLength (delta) meaning see https://docs.godotengine.org/en/stable/getting_started/first_2d_game/03.coding_the_player.html#preparing-for-collisions:~:text=The%20delta%20parameter%20in%20the%20_process()%20function%20refers%20to%20the%20frame%20length%20%2D%20the%20amount%20of%20time%20that%20the%20previous%20frame%20took%20to%20complete.%20Using%20this%20value%20ensures%20that%20your%20movement%20will%20remain%20consistent%20even%20if%20the%20frame%20rate%20changes.
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -29,16 +29,11 @@ func _process(delta): #function _process is called every frame
 	elif velocity.x < 0:
 		velocity.x += 0.5 #FRICTION!!!!!
 		
-	#if velocity.length() > 0:
-	#	velocity = velocity.normalized() * speed
-	#	$AnimatedSprite.play()
-	#else:
-	#	$AnimatedSprite.stop()
-		
 	print("X velocity: " + String(velocity.x))
 	print("Y velocity: " + String(velocity.y))
+	print("Framerate: " + String(1/frameLength))
 
-	position += velocity * delta
+	position += velocity * frameLength
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
