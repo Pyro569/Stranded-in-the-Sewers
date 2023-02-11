96b7c5ec1ee76a5476f4abbe67998c05a7a0da90
Lost Fire Game Studios <79991107+LostFireStudios@users.noreply.github.com>
2023-02-11 14:33:32 -0500

    Kind of added a level trigger

---
+++ Assets/Scripts/BlockTrigger.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func disappear():
+	print("Without you is how I disappear...")
+	self.visible = false
