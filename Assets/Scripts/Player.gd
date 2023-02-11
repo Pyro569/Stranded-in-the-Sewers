8e2c10edd5c52fd2102fa44a4b44d9ca808e66d5
Lost Fire Game Studios <79991107+LostFireStudios@users.noreply.github.com>
2023-02-10 19:39:25 -0500

    Added Moving Character
    Ohio added too

---
+++ Assets/Scripts/Player.gd
@@ -0,0 +1,46 @@
+extends Area2D
+signal hit
+
+export var speed = 400 #player speed pixels/sec EXPORT KEY ALLOWS TO SET VALUE IN INSPECTOR
+var screen_size
+
+func _ready(): #called when node enters scene tree
+	screen_size = get_viewport_rect().size
+
+
+func _process(delta): #function _process is called every frame
+	var velocity = Vector2.ZERO #player movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.x < 0:
+		$AnimatedSprite.flip_h = true #flip sprite
+	else:
+		$AnimatedSprite.flip_h = false #dont flip sprite
+
+
+func _on_Player_body_entered(body):
+	hide() #player disappears after being hit
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
