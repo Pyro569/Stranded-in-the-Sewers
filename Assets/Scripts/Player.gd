f899386d6779853945c82625f2f24a347e203666
Lost Fire Game Studios <79991107+LostFireStudios@users.noreply.github.com>
2023-02-10 22:32:20 -0500

    ADDED REAL COLLISION
    WORKS NOW

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -11,10 +11,11 @@ var maxSpeed = 300
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x = +maxSpeed	
-	if Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left"):
 		velocity.x = -maxSpeed
-	else:
+	elif is_on_floor():
 		velocity.x = 0
+
 	
 	if Input.is_action_pressed("move_up"):
 		if is_on_floor():
