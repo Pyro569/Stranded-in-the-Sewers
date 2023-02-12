c001748d7dade9644851d5e2f33ce569cac1cd4b
fenix-bear <benttonp@protonmail.com>
2023-02-11 19:32:00 -0500

    fade out

--- Assets/Scripts/BlockTrigger.gd
+++ Assets/Scripts/BlockTrigger.gd
@@ -9,11 +9,18 @@ extends Node
 var finishedCount = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.get_parent().get_parent().get_parent().get_node("Camera/Camera2D/Node2D2/ColorRect").modulate.a8 = 0
 
 func _process(delta):
 	print(finishedCount)
 	if finishedCount >= 3:
+		finishedCount = 0
+		var i = 0
+		self.get_node("CollisionShape2D/AnimatedSprite").play("default")
+		while i != 255:
+			self.get_parent().get_parent().get_parent().get_node("Camera/Camera2D/Node2D2/ColorRect").modulate.a8 += 1
+			i += 1
+			yield(get_tree().create_timer(0.01),"timeout")
 		get_tree().change_scene("res://Level2.tscn")
 		
 #unc _on
