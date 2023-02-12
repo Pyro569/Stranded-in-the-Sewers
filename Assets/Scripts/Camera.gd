844b3d4d6bed9a7f8c3e2e02b0ac204c3571d258
fenix-bear <benttonp@protonmail.com>
2023-02-11 23:15:33 -0500

    fix camera in level 2

--- Assets/Scripts/Camera.gd
+++ Assets/Scripts/Camera.gd
@@ -3,11 +3,8 @@ extends Camera2D
 export (NodePath) var Cat
 export (NodePath) var Dog
 
-func _ready():
-	position.y = 300
-
 func _process(delta):
 	Cat = get_parent().get_parent().get_node("Players").get_node("Cat").get_node("KinematicBody2D").get_global_position()
 	Dog = get_parent().get_parent().get_node("Players").get_node("Dog").get_node("KinematicBody2D").get_global_position()
-	#print(Cat.x, " ", Dog.x, " ", ((Cat.x + Dog.x) * 0.5))
+	#print(Cat.x, " ", Dog.x, " ", ((Cat.x + Dog.x) * 0.5), " ", self.position)
 	self.position = ((Cat + Dog) * 0.5)
