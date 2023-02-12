a62a4d8cc26407b2e3e5d62d80d06c1d788ee0ed
Pyro <doggolover98@outlook.com>
2023-02-12 01:39:31 -0500

    Fixed Screen Fade

--- Assets/Scripts/ScaleFade.gd
+++ Assets/Scripts/ScaleFade.gd
@@ -10,7 +10,7 @@ var screensize
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screensize = get_viewport_rect().size
-	self.set_size(screensize)
+	self.set_size(screensize) #
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
