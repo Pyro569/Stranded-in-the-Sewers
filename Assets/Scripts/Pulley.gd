837abeb3e9c44f6fc06d14239fd0d2a31b2c000b
fenix-bear <benttonp@protonmail.com>
2023-02-15 17:27:27 -0500

    pulley functional?

---
+++ Assets/Scripts/Pulley.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+var pushed = false
+
+func _process(delta):
+	print(position)
+	if position.y > -218 and pushed:
+		position.y -= 1
+	if position.y < -1017 and not pushed:
+		position.y += 1
+
+func _on_Area2D_area_entered(area):
+	pushed = true
+
+
+func _on_Area2D_area_exited(area):
+	pushed = false
